ea9fd200790ea30dce8876249bfadfb3a3d26008
Tracie Wamsley <traciewamsley@Tracies-MacBook-Pro.local>
2017-10-11 11:05:39 -0600

    added shape js but still empty

---
+++ components/Shape.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet, Platform} from 'react-native';
+
+class Shape extends Component{
+  render(){
+    return(
+      <View>
+        
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  
+
+})
+
+export default Shape;
