f1e712894de75548d97113e47e1f93d7c37bd10c
Tracie Wamsley <traciewamsley@Tracies-MacBook-Pro.local>
2017-10-16 16:52:01 -0600

    more scrolling structure

--- components/Beacon.js
+++ components/Beacon.js
@@ -20,20 +20,36 @@ class Beacon extends Component{
   }
 
   componentDidMount() {
-    // this.setState({
-    // 	name: this.props.name
-    // });
+    this.setState({
+    	name: this.props.name,
+    	clue: this.props.clue,
+    	hint: this.props.hint,
+    	address: this.props.address
+    });
   }
 
   render() {
 
     return (
-        <View>
-
-        </View>
-
+    	<View>
+  			<Image source={require('../assets/images/grid-items/gridItemBg.png')} style={styles.container}>
+  				<Text >Hello {this.props.name}</Text>
+  			</Image>
+    	</View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: 200,
+    height: 250,
+    backgroundColor:'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+
+});
 
 export default Beacon;
--- screens/GridScreen.js
+++ screens/GridScreen.js
@@ -3,30 +3,19 @@ import { Text, View, Button, Easing, TouchableOpacity, Animated, Image, StyleShe
 import { StackNavigator } from 'react-navigation';
 import { MenuContext } from 'react-native-popup-menu';
 import { Menu, MenuOptions, MenuOption, MenuTrigger } from 'react-native-popup-menu';
-import FlipView from 'react-native-flip-view'; 
+import FlipView from 'react-native-flip-view';
+
+// import beaconList from '../assets/data/beacons.js';
 
 import StatusBar from '../components/StatusBar';
+import Beacon from '../components/Beacon';
 
 class GridScreen extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = { 
-      isFlipped: false,
-      names: [
-         {'name': 'Ben', 'id': 1},
-         {'name': 'Susan', 'id': 2},
-         {'name': 'Robert', 'id': 3},
-         {'name': 'Mary', 'id': 4},
-         {'name': 'Daniel', 'id': 5},
-         {'name': 'Laura', 'id': 6},
-         {'name': 'John', 'id': 7},
-         {'name': 'Debra', 'id': 8},
-         {'name': 'Aron', 'id': 9},
-         {'name': 'Ann', 'id': 10},
-         {'name': 'Steve', 'id': 11},
-         {'name': 'Olivia', 'id': 12}
-      ]
+      
     };
   }
 
@@ -53,35 +42,14 @@ class GridScreen extends React.Component {
   _renderAll = () => {
     return (
       <Image source={require('../assets/images/bg.png')} style={styles.container}>
-          <ScrollView >
-          <Text style={{fontSize:96}}>Scroll me plz</Text>
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Text style={{fontSize:96}}>If you like</Text>
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Text style={{fontSize:96}}>Scrolling down</Text>
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Text style={{fontSize:96}}>What's the best</Text>
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Text style={{fontSize:96}}>Framework around?</Text>
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Image source={require('../assets/images/grid-items/gridItemBg.png')} />
-          <Text style={{fontSize:80}}>React Native</Text>
+        <ScrollView>
+          <Beacon name='Number 1'/>
+          <Beacon name='Number 2'/>
+          <Beacon name='Number 3'/>
+          <Beacon name='Number 4'/>
+          <Beacon name='Number 4'/>
+          <Beacon name='Number 4'/>
+          <Beacon name='qwaerstyuikjhgfdsaqwet5y6ujk'/>
         </ScrollView>
       </Image>
     );
